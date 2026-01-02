dfff680256f0e68ca9aadf1cef2affb97bff7ce8
the-air-cyborg <the.air.cyborg@gmail.com>
2026-01-02 18:25:06 +0530

    Updated login form

--- seimLite/seimLiteApp/views.py
+++ seimLite/seimLiteApp/views.py
@@ -2,22 +2,27 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
-def login_view(request):
-    if request.method=='POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        
-        user=authenticate(request,username=username,password=password)
-        
-        if user:
-            login(request,user)
-            return redirect('dashboard')
-        else:
-            return render(request,'seimLiteApp/login.html',{'error':'Invalid username or password'})
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-    return render(request,'seimLiteApp/login.html')
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "seimLiteApp/login.html", {
+        "form": form
+    })
+
 
 def signup_view(request):
     if request.method=='POST':
